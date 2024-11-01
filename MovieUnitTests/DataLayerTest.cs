ae26754a695271153e205e8123b10cf052ee957a
jeff0167 <73894404+jeff0167@users.noreply.github.com>
2024-11-01 16:45:26 +0100

    Datalayer & webService test

---
+++ MovieUnitTests/DataLayerTest.cs
@@ -0,0 +1,89 @@
+using MovieDataLayer.DataService.UserFrameworkRepository;
+
+using MovieDataLayer;
+using System.Collections;
+
+namespace MovieUnitTests
+{
+    public class DataLayerTest
+    {
+        [Fact]
+        public async Task CallAPI_UserReposity_Func_Get_ShouldGetAllUsers()
+        {
+            UserRepository userRepository = new UserRepository(new IMDBContext());
+            int expectedValue = 2;
+
+            int actualValue = (await userRepository.GetAll()).Count;
+
+            Assert.Equal(actualValue, expectedValue);
+        }
+
+        [Fact]
+        public async Task CallAPI_UserReposity_Func_Get_ShouldGetUser()
+        {
+            UserRepository userRepository = new UserRepository(new IMDBContext());
+            var expectedValue = new { Id = 1, Email = "test@ruc.dk", FirstName = "Harry" };
+
+            var actualValue = await userRepository.Get(1);
+
+            Assert.Equal(actualValue.Id, expectedValue.Id);
+            Assert.Equal(actualValue.Email, expectedValue.Email);
+            Assert.Equal(actualValue.FirstName, expectedValue.FirstName);
+        }
+
+        [Fact]
+        public async Task CallAPI_UserReposity_Func_Add_ShouldAddUser()
+        {
+            UserRepository userRepository = new UserRepository(new IMDBContext());
+            bool expectedValue = true;
+
+            bool actualValue = await userRepository.Add(new User() { Id = default, Email = "test@ruc99.dk", FirstName = "Harry", Password = "bigsecrets" });
+            User user = (await userRepository.GetAll()).Where(x => x.Email == "test@ruc99.dk").First();
+            Assert.Equal(expectedValue, actualValue);
+
+            // clean up
+            await userRepository.Delete(user.Id);
+        }
+
+        [Fact]
+        public async Task CallAPI_UserReposity_Func_Update_ShouldUpdateUser()
+        {
+            UserRepository userRepository = new UserRepository(new IMDBContext());
+            var expectedValue = new { Id = 2, Email = "test@ruc22.dk", FirstName = "Harry potter", Password = "bigsecrets" };
+
+            await userRepository.Update(new User() { Id = 2, Email = "test@ruc22.dk", FirstName = "Harry potter", Password = "bigsecrets" });
+            var actualValue = await userRepository.Get(2);
+
+            Assert.Equal(actualValue.Id, expectedValue.Id);
+            Assert.Equal(actualValue.Email, expectedValue.Email);
+            Assert.Equal(actualValue.FirstName, expectedValue.FirstName);
+
+            // clean up
+            await userRepository.Update(new User() { Id = 2, Email = "test@ruc22.dk", FirstName = "Harry", Password = "bigsecrets" });
+        }
+            
+        [Fact]
+        public async Task CallAPI_UserReposity_Func_Delete_ShouldDeleteUser()
+        {
+            UserRepository userRepository = new UserRepository(new IMDBContext());
+
+            await userRepository.Add(new User() { Id = default, Email = "test@ruc22.dk", FirstName = "Harry potter", Password = "bigsecrets" });
+            User user = (await userRepository.GetAll()).Where(x => x.Email == "test@ruc22.dk").First();
+            var success = await userRepository.Delete(user.Id);
+
+            Assert.True(success);
+        }
+        
+        //[Fact]
+        //public async Task CallAPI_UserReposity_Func_Delete_ShouldDeleteUser() // call sql :(
+        //{
+        //    UserRepository userRepository = new UserRepository(new IMDBContext());
+
+        //    await userRepository.Add(new User() { Id = default, Email = "test@ruc22.dk", FirstName = "Harry potter", Password = "bigsecrets" });
+        //    User user = (await userRepository.GetAll()).Where(x => x.Email == "test@ruc22.dk").First();
+        //    var success = await userRepository.Delete(user.Id);
+
+        //    Assert.True(success);
+        //}
+    }
+}
---
+++ MovieUnitTests/WebAPI_Test.cs
@@ -0,0 +1,69 @@
+﻿using MovieDataLayer.DataService.UserFrameworkRepository;
+using MovieDataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.Json;
+
+namespace MovieUnitTests
+{
+    public class WebAPI_Test
+    {
+        [Fact]
+        public async Task CallWebService_API_UserController_Func_GetAll_ShouldReturnOk()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            using HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7154/api/users");
+
+            HttpStatusCode statusCode = response.EnsureSuccessStatusCode().StatusCode;
+
+            Assert.Equal(statusCode, HttpStatusCode.OK);
+
+            // var jsonResponse = await response.Content.ReadAsStringAsync();
+        }
+
+        [Fact]
+        public async Task CallWebService_API_UserController_Func_GetById_ShouldReturnNotFound()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            using HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7154/api/users/9999");
+
+            HttpStatusCode statusCode = response.StatusCode;
+
+            Assert.Equal(statusCode, HttpStatusCode.NotFound);
+
+            // var jsonResponse = await response.Content.ReadAsStringAsync();
+        }
+
+        [Fact]
+        public async Task CallWebService_API_UserController_Func_Add_ShouldReturnBadRequest()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            using StringContent jsonContent = new(
+        JsonSerializer.Serialize(new
+        {
+            email = "steven@ruc.dk",
+            firstName = "stevenAlligator123794579843579873459743859353537594327597435345345345435455373773473747348374873438748737438",
+            password = "*34vrj'^2F_,d"
+        }),
+        Encoding.UTF8,
+        "application/json");
+
+            using HttpResponseMessage response = await httpClient.PostAsync("https://localhost:7154/api/users", jsonContent);
+
+            HttpStatusCode statusCode = response.StatusCode;
+
+            Assert.Equal(statusCode, HttpStatusCode.BadRequest);
+
+            // var jsonResponse = await response.Content.ReadAsStringAsync();
+        }
+
+
+    }
+}
