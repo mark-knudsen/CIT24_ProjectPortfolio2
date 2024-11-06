be7ab86a834236898f5f619d065f8bbd73106891
JacobDNielsen <jacobpeternielsen@gmail.com>
2024-11-06 09:46:55 +0100

    Update of ID check in TitleController search, and added PersonSimpleDTO (not currently in use, but would eventually be relevant another time if search introduces searches for persons

---
+++ MovieWebApi/DTO/PersonSimpleDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieWebApi.DTO
+{
+    public class PersonSimpleDTO
+    {
+        public string? Url { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
